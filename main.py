edb2c6f5d1f3f3f94ba3612958fb3a960fc324d0
jcpunzalan123 <punzalanjaycee24@gmail.com>
2025-02-28 23:47:40 +0900

    Added streamlit for the user web app

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 import boto3
+import streamlit as st
+import os
+
+## BEDROCK
+from langchain_community.embeddings import BedrockEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+
+
+## S3
+s3_client = boto3.client("s3")
+BUCKET_NAME = os.getenv("BUCKET_NAME")
+
+
+
+
+def main():
+    st.write("Chat with PDF using Amazon Bedrock!")
+    upload_file = st.file_uploader("Choose a file", "pdf")
+
+
+if __name__ == "__main__":
+    main()
