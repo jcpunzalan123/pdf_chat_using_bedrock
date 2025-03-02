59334d73ea6d978535f7a99b4d4b5cf56daf5be9
jcpunzalan123 <punzalanjaycee24@gmail.com>
2025-03-02 23:56:26 +0900

    Added codes for calling bedrock model

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import boto3
+import botocore.config
+
 import streamlit as st
+import json
 import os
 import uuid
+import response
 
 ## BEDROCK
 from langchain_community.embeddings import BedrockEmbeddings
@@ -16,6 +20,30 @@ BUCKET_NAME = os.getenv("BUCKET_NAME")
 def get_unique_id():
     return str(uuid.uuid4())
 
+
+
+def call_bedrock_model(body):
+    try:
+        bedrock=boto3.client(service_name="bedrock-runtime", region_name="us-east-1",
+                             config=botocore.config.Config(read_timeout=300, retries={'max_attempts': 3}))
+        bedrock.invoke_model(body=json.dumps(body), modelId="meta.llama3-1-8b-instruct-v1:0")
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+def analyze_pdf_using_bedrock(pdf_file):
+    prompt=f"""<s>[INST] Human: Analyze and summarize the context of {pdf_file} by writing 4-5 sentences
+    Assitant: [/INST]
+    """
+
+    body = {
+        "prompt": prompt,
+        "max_gen_len": 512,
+        "temperature": 0.5,
+        "top_p": 0.9
+    }
+
+    call_bedrock_model(body)
 
 def main():
     st.write("Chat with PDF using Amazon Bedrock!")
